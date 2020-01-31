17da8c750bd20e39cacf03fbd4c0b76fda9fd568
bbaral <baralbikram@hotmail.com>
2020-01-30 22:43:12 -0500

    Person and children demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm react app</h1>
         <p>This is really working</p>
-          <Person name='Jack' age='29' />
+          <button>Switch Name</button>
+          <Person name='Jack' age='29' >My hobbies: Racing</Person>
+          <Person name='Joe' age='23' >
+              <h1>Hello</h1>
+          </Person>
       </div>
     );
   }
