b3bfaef32547c8fddc899bf033d43bd5ccc952e5
bbaral <baralbikram@hotmail.com>
2020-02-12 21:10:45 -0500

    set style dynamically

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+import styled from "styled-components";
+
+const StyleButton = styled.button`
+            background-color: ${props => props.alt ? 'red' : 'green'};
+            color: white;
+            font: inherit;
+            border: 1px solid blue;
+            padding: 8px;
+            cursor: pointer;
+            &:hover {
+                background-color: ${props => props.alt ? 'salmon' : 'lightblue'};
+                color: black;
+            }
+`;
 
 class App extends Component {
     state = {
@@ -12,7 +25,7 @@ class App extends Component {
         ],
         otherState: 'some other value',
         showPersons: false
-    }
+    };
 
     nameChangedHandler = ( event, id ) => {
         const personIndex = this.state.persons.findIndex( p => {
@@ -23,27 +36,24 @@ class App extends Component {
             ...this.state.persons[personIndex]
         };
 
-        // const person = Object.assign({}, this.state.persons[personIndex]);
-
         person.name = event.target.value;
 
         const persons = [...this.state.persons];
         persons[personIndex] = person;
 
         this.setState( { persons: persons } );
-    }
+    };
 
     deletePersonHandler = ( personIndex ) => {
-        // const persons = this.state.persons.slice();
         const persons = [...this.state.persons];
         persons.splice( personIndex, 1 );
         this.setState( { persons: persons } );
-    }
+    };
 
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState( { showPersons: !doesShow } );
-    }
+    };
 
     render () {
         const style = {
@@ -75,11 +85,6 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            };
         }
 
         const classes = [];
@@ -91,19 +96,16 @@ class App extends Component {
         }
 
         return (
-            <StyleRoot>
                 <div className="App">
                     <h1>Hi, I'm a React App</h1>
                     <p className={classes.join( ' ' )}>This is really working!</p>
-                    <button
-                        style={style}
-                        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                    <StyleButton alt={this.state.showPersons}
+                        onClick={this.togglePersonsHandler}>Toggle Persons
+                    </StyleButton>
                     {persons}
                 </div>
-            </StyleRoot>
         );
-        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
 }
 
-export default Radium( App );
+export default App;
